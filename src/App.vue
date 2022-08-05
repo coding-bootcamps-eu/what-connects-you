6d65abf127a62a33a8431ac42ebb52b9b5c5c16c
Tobias Sindek <tobias.sindek@gmail.com>
2022-08-05 16:13:24 +0200

    feat: add feature to add names of attendees to an array

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,18 @@
-<template></template>
+<template>
+  <h1 class="app__header">What connects you?</h1>
+  <div class="name-form">
+    <input
+      type="text"
+      name="name-input"
+      id="name-input"
+      v-model="attendee.name"
+      @keypress.enter="addAttendee"
+    />
+    <label for="name-input"></label>
+    <button class="button" @click="addAttendee">Add</button>
+    <button class="button">Create Sessions</button>
+  </div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
@@ -6,6 +20,20 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "App",
   components: {},
+  data() {
+    return {
+      attendees: [{}],
+      attendee: { name: null },
+    };
+  },
+  methods: {
+    addAttendee() {
+      if (this.attendee.name) {
+        this.attendees.push({ name: this.attendee.name });
+        this.attendee.name = null;
+      }
+    },
+  },
 });
 </script>
 
