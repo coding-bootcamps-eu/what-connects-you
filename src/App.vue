e47c0a8da654b08a2f10aee0ae1f4b797d36dae1
Tobias Sindek <tobias.sindek@gmail.com>
2022-08-15 10:29:24 +0200

    feat: add 'create-session'-feature

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,42 @@
       id="name-input"
       v-model="attendee"
       @keypress.enter="addAttendee"
+      class="name-input"
     />
     <label for="name-input"></label>
-    <button class="button" @click="addAttendee">Add</button>
-    <button class="button">Create Sessions</button>
+    <button class="button" @click="addAttendee()">Add</button>
+    <button class="button" @click="createSessions()" :disabled="isValidSession">
+      Create Sessions
+    </button>
   </div>
-  <div>
-    <ol>
-      <li v-for="attendee in attendees" :key="attendee">{{ attendee }}</li>
+  <div class="added-attendees">
+    <ol class="attendee-list">
+      <li
+        v-for="attendee in attendees"
+        :key="attendee"
+        class="attendee-list__item"
+      >
+        {{ attendee }}
+      </li>
+    </ol>
+  </div>
+  <div class="sessions">
+    <ol class="sessions__list">
+      <li v-for="(session, i) in sessions" :key="i" class="sessions__list-item">
+        <h2 :class="{ finished: finishedSession === i }">
+          Session {{ i + 1 }}
+        </h2>
+        <p
+          v-for="(singleSession, j) in session"
+          :key="j"
+          :class="{ finished: finishedSession === i }"
+        >
+          {{ session[j][0] + " - " + session[j][1] }}
+        </p>
+        <button @click="toggleFinished(i)" class="button">
+          Session finished
+        </button>
+      </li>
     </ol>
   </div>
 </template>
@@ -22,6 +50,7 @@
 <script lang="ts">
 // todo: add missing unit tests
 import { defineComponent } from "vue";
+import { createSessions, Sessions } from "./round-robin-matching";
 
 export default defineComponent({
   name: "App",
@@ -30,11 +59,23 @@ export default defineComponent({
   data(): {
     attendees: string[];
     attendee: string;
+    sessions: Sessions;
+    finishedSession: number | null;
   } {
     return {
-      attendees: [],
+      attendees: ["aaaa", "bbbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh"],
       attendee: "",
+      sessions: [],
+      finishedSession: null,
     };
+  },
+  created() {
+    this.createSessions();
+  },
+  computed: {
+    isValidSession: function (): boolean {
+      return this.attendees.length > 1 && this.attendees.length % 2 !== 0;
+    },
   },
   methods: {
     addAttendee() {
@@ -43,8 +84,26 @@ export default defineComponent({
         this.attendee = "";
       }
     },
+    createSessions() {
+      this.sessions = createSessions(this.attendees);
+    },
+    toggleFinished(index: number) {
+      if (this.finishedSession === index) {
+        this.finishedSession = null;
+      } else {
+        this.finishedSession = index;
+      }
+    },
   },
 });
 </script>
 
-<style></style>
+<style>
+.sessions__list {
+  list-style: none;
+}
+
+.finished {
+  text-decoration: line-through;
+}
+</style>
